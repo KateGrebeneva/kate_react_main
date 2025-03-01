ba50b3aff026dd78cf04f2a25337c19bd1d6039e
KateGrebeneva <grebeneva.k.i.23@gmail.com>
2025-03-01 14:14:59 +0300

    task#31

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import './App.css';
+import './App.css'; // Убедитесь, что вы импортируете файл стилей
 
 function App() {
-  const isAuth = false; 
-  
+  // Вспомогательная функция для нахождения суммы цифр
+  function getDigitsSum(num) {
+    return String(num)
+      .split('')
+      .reduce((acc, digit) => acc + Number(digit), 0);
+  }
+
+  const number = 123; // Число, сумма цифр которого будет найдена
+  const sum = getDigitsSum(number); // Вычисляем сумму цифр
+
   return (
     <div className="container">
-      <h1 className="header">Добро пожаловать!</h1>
-      {/* Текст будет отображаться только если isAuth равно false */}
-      {!isAuth && <p className="text">Пожалуйста, авторизуйтесь</p>}
+      <h1 className="header">Task#31</h1>
+      <p className="text">Сумма цифр: {sum}</p>
     </div>
   );
 }
