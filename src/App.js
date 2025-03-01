51121794c4ef8367a04dc1b6062fa4b406b18782
KateGrebeneva <grebeneva.k.i.23@gmail.com>
2025-03-01 14:30:33 +0300

    task#35

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import React from 'react';
 import './App.css'; 
 
 function App() {
-  function showNumber(number) {
-    alert(number);
+  // Задание №1 - Обработчик события для вывода объекта события в консоль
+  function handleEvent(event) {
+    console.log(event); 
+  }
+
+  // Задание №2 - Обработчик события для вывода event.target в консоль
+  function handleTarget(event) {
+    console.log(event.target); 
   }
 
   return (
     <div className="container">
-      <h1 className="header">Нажмите на кнопку</h1>
-      <button className="button" onClick={() => showNumber(1)}>Click for 1</button>
-      <button className="button" onClick={() => showNumber(2)}>Click for 2</button>
-      <button className="button" onClick={() => showNumber(3)}>Click for 3</button>
+      <h1 className="header">Объект Event в React</h1>
+      <button className="button" onClick={handleEvent}>Вывести объект события</button>
+      <button className="button" onClick={handleTarget}>Вывести event.target</button>
     </div>
   );
 }
