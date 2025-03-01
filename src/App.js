63363aa3b0d641f4c7eb0e0e24a19c6e949d2356
KateGrebeneva <grebeneva.k.i.23@gmail.com>
2025-03-01 14:19:01 +0300

    task#32

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React from 'react';
-import './App.css'; // Убедитесь, что вы импортируете файл стилей
+import './App.css'; 
 
 function App() {
-  // Вспомогательная функция для нахождения суммы цифр
   function getDigitsSum(num) {
     return String(num)
       .split('')
       .reduce((acc, digit) => acc + Number(digit), 0);
   }
 
-  const number = 123; // Число, сумма цифр которого будет найдена
-  const sum = getDigitsSum(number); // Вычисляем сумму цифр
+  const number = 12345; 
+  const sum = getDigitsSum(number); 
 
   return (
     <div className="container">
-      <h1 className="header">Task#31</h1>
+      <h1 className="header">Task#32</h1>
       <p className="text">Сумма цифр: {sum}</p>
     </div>
   );
 }
 
 export default App;
+
