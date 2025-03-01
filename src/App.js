37adf6f4df0cb8330cf9dde8a57a0573af3e4381
KateGrebeneva <grebeneva.k.i.23@gmail.com>
2025-03-01 14:37:45 +0300

    task#36

--- src/App.js
+++ src/App.js
@@ -2,21 +2,41 @@ import React from 'react';
 import './App.css'; 
 
 function App() {
-  // Задание №1 - Обработчик события для вывода объекта события в консоль
-  function handleEvent(event) {
-    console.log(event); 
+  // Задача 1: Функция принимает один аргумент и выводит его в консоль
+  function func(arg) {
+    console.log('Task#36 №1: ' + arg);
   }
 
-  // Задание №2 - Обработчик события для вывода event.target в консоль
-  function handleTarget(event) {
-    console.log(event.target); 
+  // Задача 2: Функция принимает два аргумента: аргумент и объект события
+  function funcWithEvent(arg, event) {
+    console.log('Task#36 №2: ' + arg, event);
+  }
+
+  // Задача 3: Объект события передается первым параметром
+  function funcEventFirst(event, arg) {
+    console.log('Task#36 №3: ' +  event, arg);
+  }
+
+  // Задача 4: Объект события передается вторым параметром
+  function funcEventMiddle(arg1, event, arg2) {
+    console.log('Task#36 №4: ' + arg1, event, arg2);
   }
 
   return (
     <div className="container">
       <h1 className="header">Объект Event в React</h1>
-      <button className="button" onClick={handleEvent}>Вывести объект события</button>
-      <button className="button" onClick={handleTarget}>Вывести event.target</button>
+
+      <h2>Задача 1</h2>
+      <button className="button" onClick={() => func('eee')}>Click me!</button>
+
+      <h2>Задача 2</h2>
+      <button className="button" onClick={event => funcWithEvent('eee', event)}>Click me!</button>
+
+      <h2>Задача 3</h2>
+      <button className="button" onClick={event => funcEventFirst(event, 'eee')}>Click me!</button>
+
+      <h2>Задача 4</h2>
+      <button className="button" onClick={event => funcEventMiddle('arg1', event, 'arg2')}>Click me!</button>
     </div>
   );
 }
