d7df7d5999632f5ccbe47af1abfede2eb3c94894
KateGrebeneva <grebeneva.k.i.23@gmail.com>
2025-03-03 16:45:43 +0300

    task#43

--- src/App.js
+++ src/App.js
@@ -2,22 +2,33 @@ import React from 'react';
 import './App.css'; 
 
 const users = [
-  { id: 1, name: 'Elena', surn: 'Gilbert', age: 18 },
-  { id: 2, name: 'Damon', surn: 'Salvatore', age: 25 },
-  { id: 3, name: 'Catherine', surn: 'Pirs', age: 20 },
+  { id: 1, name: 'Марина', surn: 'Нарочинская', age: 30 },
+  { id: 2, name: 'Олег', surn: 'Брагин', age: 31 },
+  { id: 3, name: 'Нина', surn: 'Дубровская', age: 32 },
 ];
 
 function App() {
-  const userList = users.map((user) => (
-    <li key={user.id}>
-      {user.name} {user.surn}, Age: {user.age}
-    </li>
+  const rows = users.map((user) => (
+    <tr key={user.id}>
+      <td>{user.name}</td>
+      <td>{user.surn}</td>
+      <td>{user.age}</td>
+    </tr>
   ));
 
   return (
     <div className="container">
       <h2 className="header">Список Пользователей</h2>
-      <ul className="list">{userList}</ul>
+      <table className="user-table">
+        <thead>
+          <tr>
+            <th>Имя</th>
+            <th>Фамилия</th>
+            <th>Возраст</th>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </table>
     </div>
   );
 }
