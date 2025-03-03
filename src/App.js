debbb4b7932031614e7e28470fae74d4385d040b
KateGrebeneva <grebeneva.k.i.23@gmail.com>
2025-03-03 17:19:50 +0300

    task#49

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
+// src/App.js
 import React from 'react';
-import RandomIDGenerator from './RandomIDGenerator'; 
-import './App.css'; // Подключаем стили
+import { id } from './utils';
+import './App.css'; // Импортируем стили
 
-function App() {
+const App = () => {
+  const users = [
+    { id: id(), name: 'Alice', age: 25 },
+    { id: id(), name: 'Bob', age: 30 },
+    { id: id(), name: 'Charlie', age: 35 },
+  ];
+
   return (
-    <div className="app-container">
-      <h1 className="app-header">Генератор Уникальных ID</h1>
-      <RandomIDGenerator />
+    <div className="container">
+      <h1>Список Пользователей</h1>
+      {users.map(user => (
+        <div key={user.id} className="user">
+          ID: {user.id}, Имя: {user.name}, Возраст: {user.age}
+        </div>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
