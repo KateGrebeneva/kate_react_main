c2077895739548bdf9a0ecbc858c10739e54e64f
KateGrebeneva <grebeneva.k.i.23@gmail.com>
2025-03-05 17:40:23 +0300

    task#53

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import './App.css';
 function App() {
   const [firstName, setFirstName] = useState('Реджина');
   const [lastName, setLastName] = useState('Милс');
-  const [age, setAge] = useState(25);
 
   return (
     <div className="container">
       <h1 className="header">Данные пользователя</h1>
       <p className="text">Имя: <span>{firstName}</span></p>
+      <button className="button" onClick={() => setFirstName('Эмма')}>Изменить имя</button>
       <p className="text">Фамилия: <span>{lastName}</span></p>
-      <p className="text">Возраст: <span>{age}</span></p>
+      <button className="button" onClick={() => setLastName('Свон')}>Изменить фамилию</button>
     </div>
   );
 }
