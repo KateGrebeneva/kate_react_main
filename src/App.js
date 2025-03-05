1aace7597c995b6e4f718ec906ef500c84c08cee
KateGrebeneva <grebeneva.k.i.23@gmail.com>
2025-03-05 18:15:14 +0300

    task#58

--- src/App.js
+++ src/App.js
@@ -1,21 +1,49 @@
 import React, { useState } from "react";
-import "./App.css"; // Подключаем стили
+import "./App.css";
 
 function App() {
-  const [text, setText] = useState("");
+  const [age, setAge] = useState(0);
+  const [fahrenheit, setFahrenheit] = useState(0);
+
+  function handleAgeChange(event) {
+    setAge(event.target.value);
+  }
+
+  function handleFahrenheitChange(event) {
+    setFahrenheit(event.target.value);
+  }
+
+  const currentYear = new Date().getFullYear();
+  const birthYear = currentYear - age;
+  const celsius = (fahrenheit - 32) * (5 / 9);
 
   return (
     <div className="container">
-      <h1 className="header">Счетчик символов</h1>
+      <h1 className="header">Задание 58</h1>
 
-      <input
-        className="input"
-        type="text"
-        placeholder="Введите текст..."
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-      />
-      <p className="text">Количество символов: {text.length}</p>
+      <div className="section">
+        <h2>Определение года рождения</h2>
+        <input 
+          type="number" 
+          value={age} 
+          onChange={handleAgeChange} 
+          placeholder="Введите ваш возраст" 
+          className="input" 
+        />
+        <p className="text">Ваш год рождения: {birthYear}</p>
+      </div>
+
+      <div className="section">
+        <h2>Конвертация Фаренгейта в Цельсий</h2>
+        <input 
+          type="number" 
+          value={fahrenheit} 
+          onChange={handleFahrenheitChange} 
+          placeholder="Введите градусы Фаренгейта" 
+          className="input" 
+        />
+        <p className="text">Градусы Цельсия: {celsius.toFixed(2)}</p>
+      </div>
     </div>
   );
 }
