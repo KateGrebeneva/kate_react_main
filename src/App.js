2223feeef5bb827428b71c44bfadb76c4fe69d49
KateGrebeneva <grebeneva.k.i.23@gmail.com>
2025-03-05 17:59:48 +0300

    task#56

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css"; // Подключаем стили
 
 function App() {
-  const [age, setAge] = useState(0); // Стейт для хранения возраста
+  const [duckTales1, setDuckTales1] = useState("");
+  const [duckTales2, setDuckTales2] = useState("");
 
   return (
     <div className="container">
-      <h1 className="header">Счетчик возраста</h1>
-      <p className="text">Ваш возраст: {age}</p>
-      <button className="button" onClick={() => setAge(age + 1)}>Увеличить возраст</button>
-      <button className="button" onClick={() => setAge(age - 1)}>Уменьшить возраст</button>
+      <h1 className="header">Утиные истории</h1>
+
+      <input
+        className="input"
+        type="text"
+        placeholder="«Утиные истории» — телевизионный мультсериал, созданный компанией Уолта Диснея."
+        value={duckTales1}
+        onChange={(e) => setDuckTales1(e.target.value)}
+      />
+      <p className="text">Текст 1: {duckTales1}</p>
+
+      <input
+        className="input"
+        type="text"
+        placeholder="Главными героями мультсериала являются пожилой селезень Скрудж МакДак и его племянники-утята — Билли, Вилли и Дилли."
+        value={duckTales2}
+        onChange={(e) => setDuckTales2(e.target.value)}
+      />
+      <p className="text">Текст 2: {duckTales2}</p>
     </div>
   );
 }
