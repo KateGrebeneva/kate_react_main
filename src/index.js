eff94bbbc97280df27f958ae57be4ecdab7d1b81
KateGrebeneva <grebeneva.k.i.23@gmail.com>
2025-03-13 13:50:06 +0300

    task#95_1

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Users from './Users';
+import Calculator from './Calculator';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Users/>
+    <Calculator/>
   </React.StrictMode>
 );
 
