8c3ed43ff3f13375760e7d66611802116dcfc1a7
KateGrebeneva <grebeneva.k.i.23@gmail.com>
2025-03-13 14:32:42 +0300

    task#98

--- src/App.js
+++ src/App.js
@@ -1,47 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
 
-function App() {
-  const [notes, setNotes] = useState([1, 2, 3, 4, 5]);
-  const [editNum, setEditNum] = useState(null);
-
-  const result = notes.map((note, index) => {
-    return (
-      <div
-        key={index}
-        className="note-container"
-        onClick={() => startEdit(index)}
-      >
-        <p className="note-text">{note}</p>
-      </div>
-    );
-  });
-
-  function startEdit(index) {
-    setEditNum(index);
-  }
-
-  function changeItem(event) {
-    const updatedNotes = [...notes];
-    updatedNotes[editNum] = event.target.value;
-    setNotes(updatedNotes);
-  }
-
+function DuckTales() {
   return (
-    <div className="app-container">
-      <div className="notes-list">{result}</div>
-      {editNum !== null && (
-        <div className="input-container">
-          <input
-            className="edit-input"
-            value={notes[editNum]}
-            onChange={changeItem}
-            autoFocus
-          />
-        </div>
-      )}
+    <div className="container">
+      <h1 className="title">Утиные Истории</h1>
+      <button className="btn1">Начать смотреть</button>
+      <button className="btn2">Посмотреть трейлер</button>
     </div>
   );
 }
 
-export default App;
+export default DuckTales;
